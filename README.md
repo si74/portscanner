085186c779d00eb4ec0a61d9064bcedd45a76342
Sneha Inguva <singuva@digitalocean.com>
2018-07-09 12:02:31 -0600

    portscanner: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # portscanner
 test port scanner
+
+## Background
+
+nmap is a utility that scans remote network hosts and attempts to open
+TCP/UDP connections to determine open ports.
+
+This program is a similar. It attempts to open connections to determine
+open ports. It also leverages a semaphore to limit the number of concurrent
+connections. 
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+func main() {
+
+	hostname := "localhost"
+
+	// worker pool channel
+	conSema := make(chan struct{}, 10)
+
+	var wg sync.WaitGroup
+	for i := 1; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			conSema <- struct{}{}
+			addr := fmt.Sprintf("%s:%d", hostname, port)
+			conn, err := net.Dial("tcp", addr)
+			if err != nil {
+				// handle error
+				//fmt.Printf("port %d closed: %v\n", port, err)
+			} else {
+				fmt.Printf("port %d open\n", port)
+				conn.Close()
+			}
+			<-conSema
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+}
