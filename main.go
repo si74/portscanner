b5496943f20a6f36939b61dd557e602bb027e1fa
Sneha Inguva <singuva@digitalocean.com>
2020-09-08 13:57:46 -0400

    semaphore fix

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func main() {
 	var wg sync.WaitGroup
 	for i := 1; i < 65535; i++ {
 		wg.Add(1)
+		conSema <- struct{}{}
 		go func(port int) {
-			conSema <- struct{}{}
 			addr := fmt.Sprintf("%s:%d", hostname, port)
 			conn, err := net.Dial("tcp", addr)
 			if err != nil {
 				// handle error
-				//fmt.Printf("port %d closed: %v\n", port, err)
+				fmt.Printf("port %d closed: %v\n", port, err)
 			} else {
 				fmt.Printf("port %d open\n", port)
 				conn.Close()
